6724b6ef3558d1917d8d9d90ce530f12502d0c8a
Sumyabazar <bazarbazar821@gmail.com>
2023-07-03 21:29:10 +0800

    make the dice rollable

--- app.js
+++ app.js
@@ -8,15 +8,21 @@ var scores = [0, 0];
 var roundScore = 0;
 
 //Шоо аль талаараа буусныг хадгалах сав хэрэгтэй, 1-6 гэсэн хувьсагчийг энэ хувьсагчинд санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor(Math.random() * 6) + 1;
-var dice1 = Math.floor(Math.random() * 6) + 1;
-window.document.querySelector("#score-0").textContent = dice;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+//window.document.querySelector("#score-0").textContent = dice;
 // document.querySelector("#score-1").textContent = dice1;
 // document.querySelector("#score-1").innerHTML = "<em>Yes!<em>";
-window.document.querySelector("#score-0").textContent = 0;
-window.document.querySelector("#score-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
-window.document.querySelector("#current-0").textContent = 0;
-window.document.querySelector("#current-1").textContent = 0;
+// Программ эхлэхэд бэлтгэе
+window.document.getElementById("score-0").textContent = "0";
+window.document.getElementById("score-1").textContent = "0";
 
-console.log("Шоо :" + dice);
+window.document.getElementById("current-0").textContent = "0";
+window.document.getElementById("current-1").textContent = "0";
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function () {
+  var diceNumber = Math.floor(Math.random() * 6) + 1;
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
