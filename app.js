4b10e5a08ba9813d63f79bf0a25515c0524f1c3f
Sumyabazar <bazarbazar821@gmail.com>
2023-07-03 22:35:13 +0800

    toglogvhiin idewhtwei baidal onoog tsugluulah zergiig oruullaa

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах сав
 var scores = [0, 0];
@@ -22,7 +22,36 @@ window.document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
 document.querySelector(".btn-roll").addEventListener("click", function () {
+  // 1-6 хүртэлх тоог санамсаргүйгээр сонгоно
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Шооны зургийг веб дээр гаргаж ирнэ
   diceDom.style.display = "block";
+  // Буусан шооны тоонд тохирсон шооны зургийг веб дээр гаргаж ирнэ
   diceDom.src = "dice-" + diceNumber + ".png";
+  // Буусан тоо нь 1 ээс ялгаатай бол бууан шооны тоог Тоглогчийн ээлжийн тоог нэмнэ
+  if (diceNumber !== 1) {
+    //1 ээс ялгаатай тоо буулаа.Буусан тоог тоглогчид нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    //1 буусан тул тоглогчийн ээлжийг энэ хэсэгт сольж өгнө.
+    // хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+    // үгүй бол идэвхтэй тоглогчийг 1 болго.
+    //Энэ тоглогчийн цуглуулсан оноог 0 болгоно.
+    //Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+    // Улаан цэгийг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // Идэвхтэй тоглогч шилжих үед шооны зургийг алга болгоно.
+    diceDom.style.display = "none";
+  }
 });
+// Шоог шидэх эвент листенер
